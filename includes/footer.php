b7358fa963c8c5040eb72b0f6a796bbdbf9c75ce
LorianaD <loriana.diano@gmail.com>
2025-08-25 16:08:54 +0200

    add : remplisage du header et du footer, et ajout de la page de base de la bibliotheque

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footerContainer">
+    <p class="copyright">© 2025 - Groupe LADT</p>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header class="headerContainer">
+    <h1 class="title-site">La librairie</h1>
+    <nav class="nav-site">
+        <ul class="list-btn-site">
+            <li class="btn-nav-site"><a href="../public/index.php">Accueil</a></li>
+            <li class="btn-nav-site"><a href="../public/library.php">Bibliothéque</a></li>
+        </ul>
+    </nav>
+</header>
